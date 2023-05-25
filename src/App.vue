a43b8584192522898e1ac4dbf5cbc1b9e64bfafc
Chenxyan99 <251601918@qq.com>
2023-05-25 16:31:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+ 
+<script setup>
+import * as Cesium from 'cesium';
+import { onMounted } from 'vue';
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer');
+});
+</script>
+ 
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+html,body,#cesiumContainer{
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+</style>
